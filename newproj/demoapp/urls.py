6c0bf513dbccfabfe9fc5b395e38b434c787281d
anaghasynnefo <anaghasynnefo@gmail.com>
2023-12-30 08:54:55 +0530

    adding files

---
+++ newproj/demoapp/urls.py
@@ -0,0 +1,43 @@
+from django.urls import path
+from.import views
+urlpatterns =[
+    path('',views.home,name='home'),
+    path('about/',views.about,name='home')
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
